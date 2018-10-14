b93de5512b2a1501793410c3e2b8f417a8fb6b94
Burak Calisgan <calisganburak@gmail.com>
2018-10-15 01:46:46 +0300

    Initial commit

---
+++ app/src/main/java/burakcalisgan/com/buttontest/MainActivity.java
@@ -0,0 +1,28 @@
+package burakcalisgan.com.buttontest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    
+
+
+
+    /*buton onclick metodu
+    public void calistir (View view){
+        if ( view.getId() == R.id.button)
+            Log.e("Button", "1. Butona Tıklandı !");
+        else
+            Log.e("Button", "2. Butona Tıklandı !");
+
+    }*/
+}
